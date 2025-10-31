65f30d1f34fef78b1d75d05881392b413bd761e9
jundiramadhan95 <jundi1995@gmail.com>
2025-10-31 09:52:39 +0700

    edit app.py

--- app.py
+++ app.py
@@ -160,4 +160,4 @@ def telegram_webhook():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
